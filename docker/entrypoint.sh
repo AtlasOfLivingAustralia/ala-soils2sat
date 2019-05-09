ded150a82b8af1bcc3c491122751a7a10e0b84c1
Tom Saleeba <tom.saleeba@gmail.com>
2019-05-09 22:44:17 +0930

    fix: drop back to postgres 9.4 to avoid another error with 9.6, add healthcheck to app, add autoheal container to bounce on failed healthcheck, actually get tomcat memory settings working

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -19,6 +19,11 @@ sed -i "s/password = \"\w\+\"/password = \"${DBPASS:?}\"/" $dsFile
 sed -i "s+aekosServiceRoot = \"\w\+\"+aekosServiceRoot = \"${DEFAULT_AEKOS_SERVICES_URL:?}\"+" $configFile
 sed -i "s+doiS2SRoot = \"\w\+\"+doiS2SRoot = \"${DEFAULT_DOI_S2S_ROOT_URL:?}\"+" $configFile
 
+# see https://grails.github.io/grails2-doc/2.2.1/ref/Command%20Line/run-war.html
+cat <<EOF >> $confDir/BuildConfig.groovy
+grails.tomcat.jvmArgs= ["-Xmx${TOMCAT_MAX_MEM_MB:-1024}m", "-XX:MaxPermSize=${TOMCAT_MAX_PERM_MB:-512}m"]
+EOF
+
 # it would be nice if logs we written to stdout, but they aren't :(
 # FIXME need to 'forever' the tail processes so they restart if failed, or just get tomcat to write to stdout
 outFile=target/tomcat-out.txt
@@ -37,5 +42,4 @@ echo "[INFO] logging may not work, the log files live at out=$outFile, err=$errF
 
 grails \
   -Dgrails.env=$grailsEnv \
-  -Dgrails.tomcat.jvmArgs="${TOMCAT_JVM_ARGS:-}" \
   $@
