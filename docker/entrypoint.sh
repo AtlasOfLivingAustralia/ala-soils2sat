a93c2145d2b698d9298f16fa5e6cc1b7c9ce48ad
Tom Saleeba <tom.saleeba@gmail.com>
2019-05-10 14:03:06 +0930

    chore: get the sed commands actually working for URLs in the entrypoint.sh, change the value for DOI root URL as we don't control the .ala. one

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,14 +10,14 @@ configFile=$confDir/Config.groovy
 
 # overwrite all the DB connection details, for all envs
 sed -i "s+postgresql://.*\"+postgresql://${DBHOST:?}/${DBNAME:?}\"+" $dsFile
-sed -i "s/username = \"\w\+\"/username = \"${DBUSER:?}\"/" $dsFile
-sed -i "s/password = \"\w\+\"/password = \"${DBPASS:?}\"/" $dsFile
+sed -i "s/username = \".*/username = \"${DBUSER:?}\"/" $dsFile
+sed -i "s/password = \".*/password = \"${DBPASS:?}\"/" $dsFile
 
 # config (managed in SettingService.groovy) is stored in the DB, you're just setting the *defaults* here
 # you can configure all settings in the web UI by accessing http://localhost:8080/ala-soils2sat/admin/settings as an
 # admin
-sed -i "s+aekosServiceRoot = \"\w\+\"+aekosServiceRoot = \"${DEFAULT_AEKOS_SERVICES_URL:?}\"+" $configFile
-sed -i "s+doiS2SRoot = \"\w\+\"+doiS2SRoot = \"${DEFAULT_DOI_S2S_ROOT_URL:?}\"+" $configFile
+sed -i "s+aekosServiceRoot = \".*+aekosServiceRoot = \"${DEFAULT_AEKOS_SERVICES_URL:?}\"+" $configFile
+sed -i "s+doiS2SRoot = \".*+doiS2SRoot = \"${DOI_S2S_ROOT_URL:?}\"+" $configFile
 
 # see https://grails.github.io/grails2-doc/2.2.1/ref/Command%20Line/run-war.html
 cat <<EOF >> $confDir/BuildConfig.groovy
